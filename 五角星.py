07df603b819a1816d654675061a24d5a4542f40f
CSDNmanuscript <40167309+CSDNmanuscript@users.noreply.github.com>
2018-07-02 11:11:55 +0800

    Add files via upload

---
+++ 五角星.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.forward(400)
+turtle.right(144)
+turtle.forward(400)
+turtle.right(144)
+turtle.forward(400)
+turtle.right(144)
+turtle.forward(400)
+turtle.right(144)
+turtle.forward(400)
