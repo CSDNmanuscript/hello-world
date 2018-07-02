48d8f58f1e17e64e0ab8168daf4d6d8012d8c3a0
CSDNmanuscript <40167309+CSDNmanuscript@users.noreply.github.com>
2018-07-02 11:23:02 +0800

    Create enter_name.py

---
+++ enter_name.py
@@ -0,0 +1,12 @@
+name =input("enter your name:\n")
+print("your name is\40"+name+"\40.")
+
+#                   python的关键字
+#   and                 elif                import              return
+#   as                  else                in                  try
+#   assert              except              is                  while
+#   break               finally             lambda              with
+#   class               for                 not                 yield
+#   continue            from                or
+#   def                 global              pass
+#   del                 if                  raise
